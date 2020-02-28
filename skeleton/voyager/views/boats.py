6ab747ae8897f927cf18c9b90615ec4ed14bb1e1
Pujan Thapa <42918326+pujan01@users.noreply.github.com>
2020-02-27 21:57:11 -0800

    boats sailed by a specific sailor done

--- skeleton/voyager/views/boats.py
+++ skeleton/voyager/views/boats.py
@@ -7,8 +7,6 @@ from flask import escape
 
 from voyager.db import get_db, execute
 
-def boats(conn):
-    return execute(conn, "SELECT b.bid, b.name, b.color FROM Boats AS b")
 
 def views(bp):
     @bp.route("/boats")
@@ -16,3 +14,17 @@ def views(bp):
         with get_db() as conn:
             rows = boats(conn)
         return render_template("table.html", name="boats", rows=rows)
+
+    @bp.route("/boats/sailed-by")
+    def _get_boats_from_sailor_name():
+        with get_db() as conn:
+            sailor_name = request.args.get("sailor-name")
+            rows = get_boats_from_sailor_name(conn, sailor_name)
+        return render_template("table.html", name="Boats", rows=rows)
+
+
+def boats(conn):
+    return execute(conn, "SELECT b.bid, b.name, b.color FROM Boats AS b")
+
+def get_boats_from_sailor_name(conn, sailor_name):
+    return execute(conn, "SELECT b.bid, b.name, b.color FROM Sailors s , Voyages v, Boats b WHERE b.name = boat_name, s.sid = v.sid, v.bid = b.bid", {'sailor_name': sailor_name })
--- skeleton/voyager/views/sailors.py
+++ skeleton/voyager/views/sailors.py
@@ -17,19 +17,18 @@ def views(bp):
             rows = sailors(conn)
         return render_template("table.html", name="Sailors", rows=rows)
         
-    # @bp.route("/sailors/who-sailed")
-    # def _get_sailors_from_boat_name():
-    #     print("hewe")
-    #     with get_db() as conn:
-    #         boat_name = request.args.get("boat-name")
-    #         print(boat_name)
-    #         rows = get_sailors_from_boat_name(conn, boat_name)
-    #     return render_template("table.html", name="Sailors", rows=rows)
+    @bp.route("/sailors/who-sailed")
+    def _get_sailors_from_boat_name():
+        with get_db() as conn:
+            boat_name = request.args.get("boat-name")
+            rows = get_sailors_from_boat_name(conn, boat_name)
+        return render_template("table.html", name="Sailors who sailed on " + boat_name, rows=rows)
 
 def sailors(conn):
     return execute(conn, "SELECT s.sid, s.name, s.age, s.experience FROM Sailors AS s")
 
-# def get_sailors_from_boat_name(conn, boat_name):
-#     #return execute(conn, "SELECT b.name FROM Boats b where b.name = b.name" )
-#     #return execute(conn, "SELECT s.sid, s.name, s.age, s.experience FROM Sailors s , Voyages v, Boats b WHERE  b.name = :boat_name", {'boat_name': bname }, s.sid = v.sid, v.bid = b.bid")
+def get_sailors_from_boat_name(conn, boat_name):
+    return execute(conn, "SELECT s.sid, s.name, s.age, s.experience, b.name, v.date_of_voyage FROM Sailors s , Voyages v, Boats b WHERE  b.name = :b_name AND s.sid = v.sid AND v.bid = b.bid", {'b_name': boat_name })
 
+# WHERE Voyages.date_of_voyage = :v_date", {'v_date': voyage_date})
+# func (conn, "Select ... FROM ... WHERE Voyages.date_of_voyage = :v_date",  {'v_date': voyage_date})
