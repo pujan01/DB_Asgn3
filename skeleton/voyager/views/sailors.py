04880bb06eef5c797ae4e1aff2c80efe93dc4564
Pujan Thapa <42918326+pujan01@users.noreply.github.com>
2020-02-27 21:24:37 -0800

    made changes to get all the sailors and voyages

--- skeleton/voyager/views/sailors.py
+++ skeleton/voyager/views/sailors.py
@@ -11,7 +11,25 @@ from voyager.validate import NAME_RE, INT_RE, DATE_RE
 
 
 def views(bp):
-
     @bp.route("/sailors")
     def _get_all_sailors():
-        return 'not implemented'
+        with get_db() as conn:
+            rows = sailors(conn)
+        return render_template("table.html", name="Sailors", rows=rows)
+        
+    @bp.route("/sailors/who-sailed")
+    def _get_sailors_from_boat_name():
+        print("hewe")
+        with get_db() as conn:
+            boat_name = request.args.get("boat-name")
+            print(boat_name)
+            rows = get_sailors_from_boat_name(conn, boat_name)
+        return render_template("table.html", name="Sailors", rows=rows)
+
+def sailors(conn):
+    return execute(conn, "SELECT s.sid, s.name, s.age, s.experience FROM Sailors AS s")
+
+def get_sailors_from_boat_name(conn, boat_name):
+    #return execute(conn, "SELECT b.name FROM Boats b where b.name = b.name" )
+    return execute(conn, "SELECT s.sid, s.name, s.age, s.experience FROM Sailors s , Voyages v, Boats b WHERE  b.name = :boat_name", {'boat_name': bname }, s.sid = v.sid, v.bid = b.bid")
+
